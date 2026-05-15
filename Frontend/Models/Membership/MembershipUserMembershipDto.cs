e8704aa0c77561c4a1102da3d34b36a0f5010889
MortenStaunstrup <mort490fgromling@gmail.com>
2026-05-15 11:27:09 +0200

    Added connection with MembershipService and logout logic for profile page
    # Conflicts:
    #	Frontend/Pages/ProfilePage.razor
    #	Frontend/Services/UserService.cs

---
+++ Frontend/Models/Membership/MembershipUserMembershipDto.cs
@@ -0,0 +1,7 @@
+namespace Frontend.Models.Membership;
+
+public class MembershipUserMembershipDto
+{
+    public Subscription Subscription { get; set; }
+    public MemberSubscription MemberSubscription { get; set; }
+}
--- Frontend/Services/MembershipService.cs
+++ Frontend/Services/MembershipService.cs
@@ -1,6 +1,47 @@
-﻿namespace Frontend.Services;
+﻿using System.Net.Http.Headers;
+using Frontend.Models.Membership;
+
+namespace Frontend.Services;
 
 public class MembershipService
 {
+    
+    private readonly HttpClient _httpClient;
+    public MembershipService(IHttpClientFactory httpClient)
+    {
+        _httpClient = httpClient.CreateClient("MembershipClient");
+    }
+
+    public async Task<MembershipUserMembershipDto?> GetMemberSubscriptionById(int memberId, string jwt)
+    {
+        _httpClient.DefaultRequestHeaders.Clear();
+        _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", jwt);
+        
+        var result = await _httpClient.GetAsync($"MemberSubscription/{memberId}");
+        if (result.IsSuccessStatusCode)
+        {
+            var userMemberShip = await result.Content.ReadFromJsonAsync<MemberSubscription>();
+            if (userMemberShip == null)
+                return null;
+            
+            var subscriptionResult = await _httpClient.GetAsync($"subscription/{userMemberShip.MemberId}");
+            if (subscriptionResult.IsSuccessStatusCode)
+            {
+                var subscription =
+                    await _httpClient.GetFromJsonAsync<Subscription>($"subscription/{userMemberShip.SubscriptionId}");
+                if (subscription == null)
+                    return null;
+
+                var dto = new MembershipUserMembershipDto()
+                {
+                    Subscription = subscription,
+                    MemberSubscription = userMemberShip
+                };
+                
+                return dto;
+            }
+        }
+        return null;
+    }
     
 }
